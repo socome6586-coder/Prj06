7f0d353e7189c63578e222fe0237428d0e7c3a20
socome6586-coder/Seok-dev <socome6586@gmail.com>
2026-03-26 11:41:26 +0900

    ex01, TestScore 학점 수정

--- src/ex01/TestScore.java
+++ src/ex01/TestScore.java
@@ -53,21 +53,59 @@ class Score {
 		this.avg  =  (this.kor + this.eng + this.mat) / 3.0;
 	}
 	
+	// ctrl + shift + p : 대응하는 {} 를 찾아준다
 	private void getGrade() {
-		this.grade = 'A';		
-	}	
-
+		
+		int v = (int) this.avg / 10;
+		switch(v) {
+		case 10 : 
+		case 9 : this.grade = 'A'; break;
+		case 8 : this.grade = 'B'; break;
+		case 7 : this.grade = 'C'; break;
+		case 6 : this.grade = 'D'; break;
+		default : this.grade = 'F'; break; 
+		}
+		
+	}
+	
+	
+	/* if 방식
+	private void getGrade() {
+		if(90 <=this.avg && this.avg <= 100) {
+			this.grade = 'A';
+		} else {
+			if(80 <=this.avg && this.avg <= 90) {
+				this.grade = 'B';
+			} else {
+				if(70 <=this.avg && this.avg <= 80) {
+					this.grade = 'C';
+				} else {
+					if(60 <=this.avg && this.avg <= 70) {
+						this.grade = 'D';
+					}else {
+						if(0 <= this.avg && this.avg <= 60) {
+							this.grade = 'F';
+						} else {
+							this.grade = ' ';
+						}
+					}
+				}
+			}
+		}
+	}
+	*/ // getGrade() end
+	
 	static void title() {
 		String  title = "번호 이름 국어 영어 수학 총점 평균  등급";
 		System.out.println( title );		
-	}
+	} // title end
 	
 	// Method
 	void disp() {
 		String  fmt   = "%d   %s   %d   %d   %d   %d   %.2f  %c\n";
 		System.out.printf(fmt, num, name, kor, eng, mat, tot, avg, grade);
-	}	
-}
+	}	// disp end
+} // class end
 
 
 public class TestScore {
@@ -83,6 +121,9 @@ public class TestScore {
 		
 		Score  sc2 = new Score(2,"모모",0,70,100);
 		sc2.disp();
+
+		Score  sc3 = new Score(3,"원영",90,90,100);
+		sc3.disp();
 
 	}
 
